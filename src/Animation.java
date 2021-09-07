3fd615b26ad3f0a30f6eba932f37b26842c1305a
021910003 <issaghaly0@gmail.com>
2021-09-07 17:40:25 +0200

    ça avance

--- src/Animation.java
+++ src/Animation.java
@@ -2,14 +2,18 @@ import java.awt.Event;
 
 public class Animation {
     JeuSimple jeu;
+    boolean[][] tab= new boolean[5][5];
 
     public Animation(JeuSimple jeu) {
         this.jeu = jeu;
     }
 
     public void run(int d) throws InterruptedException {
+        tab[2][1]=true;
+        tab[3][1]=true;
+        tab[2][2]=true;
         System.out.print((char) Event.ESCAPE + "7");
-        jeu.reset();
+        //jeu.reset();
 
         for (int i = 0; i < d; i++) {
             System.out.print((char) Event.ESCAPE + "[2J");
@@ -17,7 +21,7 @@ public class Animation {
             System.out.print("");
             System.out.print(jeu);
             System.out.print("t = " + i);
-            jeu.succ();
+            jeu.succ(tab);
             Thread.sleep(200);
         }
     }
--- src/JeuSimple.java
+++ src/JeuSimple.java
@@ -1,20 +1,37 @@
-public class JeuSimple {
+public class JeuSimple {/*
     private boolean etat;
+    private int posX;
+    private int posY;*/
 
-    public boolean succ(boolean[][] tab, int positionX, int positionY) {
-        int mort = 8;
-        for (int horizontale = -1; horizontale <= 1; horizontale++) {
-            for (int verticale = -1; verticale <= 1; verticale++) {
-                if (tab[(positionX + verticale)][(positionY + horizontale)]) {
-                    mort--;
+    public JeuSimple() {
+    }
+
+    ;
+
+    public void succ(boolean[][] tab) {
+        int mort = -1;
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[i].length; j++) {
+                mort = 8;
+                for (int horizontale = -1; horizontale <= 1; horizontale++) {
+                    for (int verticale = -1; verticale <= 1; verticale++) {
+                        if (((i + verticale) < 0 || i + verticale >= tab.length) || ((j + horizontale) < 0) || j + horizontale >= tab[i].length) {
+                            mort--;
+                        } else {
+                            if (tab[(i + verticale)][(j + horizontale)]) {
+                                mort--;
+                            }
+                        }
+                    }
                 }
+                tab[i][j] = (mort == 6 && tab[i][j]) || mort == 5;
             }
         }
-
-        return (mort == 6 && tab[positionX][positionY]) || mort == 5;
+        // return (mort == 6 && tab[this.posX][this.posX]) || mort == 5;
     }
 
     public String toString() {
-        return etat ? "+" : "_";
+
+        return "";
     }
 }
